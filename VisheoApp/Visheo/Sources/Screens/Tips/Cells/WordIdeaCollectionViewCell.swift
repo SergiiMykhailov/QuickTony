c717439635538b2277057faf2853e7d3f6d1868f
Nikita Ivanchikov <nivanchikov.dev@gmail.com>
2017-12-29 12:15:36 +0200

    VIS-159 - Add pagination to word ideas

--- VisheoApp/Visheo/Sources/Screens/Tips/Cells/WordIdeaCollectionViewCell.swift
+++ VisheoApp/Visheo/Sources/Screens/Tips/Cells/WordIdeaCollectionViewCell.swift
@@ -12,15 +12,6 @@ class WordIdeaCollectionViewCell: UICollectionViewCell {
     
 	@IBOutlet weak var textLabel: UILabel!
 	
-	override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
-		var frame = layoutAttributes.frame;
-		frame.origin.y = 0.0;
-		
-		let attributes = layoutAttributes.copy() as! UICollectionViewLayoutAttributes;
-		attributes.frame = frame;
-		return attributes;
-	}
-	
 	func setup(with model: WordTipCellModel) {
 		textLabel.text = model.text;
 	}
--- VisheoApp/Visheo/Sources/Screens/Tips/TipsCollectionViewMediator.swift
+++ VisheoApp/Visheo/Sources/Screens/Tips/TipsCollectionViewMediator.swift
@@ -14,6 +14,8 @@ class TipsCollectionViewMediator: NSObject, UICollectionViewDataSource, UICollec
 	private let practicesCollectionView: UICollectionView;
 	private let viewModel: TipsViewModel;
 	private let containerWidth: CGFloat;
+	var currentPage: Int = 0
+	let itemsSpacing : CGFloat = 10
 	
 	init(viewModel: TipsViewModel, wordIdeasCollectionView: UICollectionView, practicesCollectionView: UICollectionView, containerWidth: CGFloat) {
 		self.viewModel = viewModel;
@@ -23,13 +25,11 @@ class TipsCollectionViewMediator: NSObject, UICollectionViewDataSource, UICollec
 		
 		super.init();
 		
-		let dimension = ceil(containerWidth * 0.76);
 		let wordsLayout = AlignedFlowLayout(alignment: .top);
 		wordsLayout.minimumInteritemSpacing = 10.0;
-		wordsLayout.minimumLineSpacing = 10.0;
+		wordsLayout.minimumLineSpacing = 0.0;
 		wordsLayout.scrollDirection = .horizontal;
 		wordsLayout.sectionInset = UIEdgeInsetsMake(0.0, 20.0, 0.0, 20.0);
-		wordsLayout.estimatedItemSize = CGSize(width: dimension, height: dimension);
 		
 		wordIdeasCollectionView.collectionViewLayout = wordsLayout;
 		wordIdeasCollectionView.delegate = self;
@@ -54,7 +54,7 @@ class TipsCollectionViewMediator: NSObject, UICollectionViewDataSource, UICollec
 		
 		var insets = layout.sectionInset;
 		if viewModel.numberOfItems(for: .words) < 2 {
-			insets.left = (containerWidth - layout.estimatedItemSize.width) / 2.0;
+			insets.left = (containerWidth - wordPageWidth) / 2.0;
 		} else {
 			insets.left = 20.0;
 		}
@@ -91,5 +91,53 @@ class TipsCollectionViewMediator: NSObject, UICollectionViewDataSource, UICollec
 		}
 		
 		return UICollectionViewCell();
+	}
+	
+	var wordPageWidth : CGFloat {
+		return containerWidth - 3 * itemsSpacing
+	}
+	
+	func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+		if scrollView != wordIdeasCollectionView { return }
+		currentPage = Int(floor((wordIdeasCollectionView.contentOffset.x - wordPageWidth / 2) / wordPageWidth) + 1)
+	}
+	
+	func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+		if scrollView != wordIdeasCollectionView { return }
+		
+		var newPage = currentPage
+		
+		if velocity.x == 0 {
+			newPage = Int(floor((targetContentOffset.pointee.x - wordPageWidth / 2) / wordPageWidth) + 1);
+		}
+		else {
+			newPage = velocity.x > 0 ? currentPage + 1 : currentPage - 1;
+			if newPage < 0 {
+				newPage = 0
+			}
+			if newPage > Int(wordIdeasCollectionView.contentSize.width / wordPageWidth) {
+				newPage = Int(ceil(wordIdeasCollectionView.contentSize.width / wordPageWidth)) - 1
+			}
+		}
+		
+		var offset: CGFloat = 0.0;
+		if let layout = wordIdeasCollectionView.collectionViewLayout as? UICollectionViewFlowLayout, let frame = layout.layoutAttributesForItem(at: IndexPath(item: newPage, section: 0))?.frame {
+			offset = frame.origin.x
+			offset -= layout.sectionInset.left;
+		}
+
+		targetContentOffset.pointee = CGPoint(x: offset, y: targetContentOffset.pointee.y)
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		switch collectionView {
+			case wordIdeasCollectionView:
+				let holidayItemSide = containerWidth - 4 * itemsSpacing
+				return CGSize(width: holidayItemSide, height: holidayItemSide)
+			case practicesCollectionView:
+				return CGSize(width: containerWidth, height: 50.0);
+			default:
+				return CGSize.zero;
+		}
 	}
 }
