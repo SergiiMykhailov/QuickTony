80340fbde9f616bf2cb6ec26843f0f49ce1248e4
Ivan Kramarenko <i.kramarenko@olearis.com>
2018-04-27 12:57:52 +0300

    [FIX] (Occasions): long loading activity indicator (VIS-237)

--- VisheoApp/Visheo/Sources/Screens/ChooseOccassion/Cells/CollectionViewCells/HolidayCollectionViewCell.swift
+++ VisheoApp/Visheo/Sources/Screens/ChooseOccassion/Cells/CollectionViewCells/HolidayCollectionViewCell.swift
@@ -15,11 +15,16 @@ class HolidayCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var holidayCoverImage: UIImageView!
     @IBOutlet weak var holidayDateLabel: UILabel!
     @IBOutlet weak var holidayFreeLabel: BadgeSwift!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     func setup(with viewModel: HolidayCellViewModel) {
 		holidayDateLabel.isHidden = !viewModel.displaysDate;
         holidayDateLabel.text = viewModel.holidayDateText
-        holidayCoverImage.sd_setImage(with: viewModel.imageURL, completed: nil)
         holidayFreeLabel.isHidden = !viewModel.isFree
-    }    
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        holidayCoverImage.sd_setImage(with: viewModel.imageURL) { [weak self] _,_,_,_ in
+            self?.activityIndicator.isHidden = true
+        }
+    }
 }
